030f7fa7bc456bcd327eb81fc54eba23f4fb1c14
Elaf Moataz <elafmoataz7@gmail.com>
2022-12-26 22:20:23 +0200

    adding space

--- Vodafone_shop/src/main/java/Pages/BasePage.java
+++ Vodafone_shop/src/main/java/Pages/BasePage.java
@@ -14,6 +14,7 @@ public class BasePage {
         browser = desiredBrowser;
     }
 
+
     public void waitElement(By element, int time){
         WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(time));
         wait.until(ExpectedConditions.visibilityOfElementLocated(element));
